0062756e09bc7ec380b2a278ed373148c10a35fe
rhysx19 <rhysx19@users.noreply.github.com>
2026-07-23 22:03:03 +0000

    Disambiguate WPF vs WinForms/Drawing types via global usings

---
+++ src/RoundLaunchpad/GlobalUsings.cs
@@ -0,0 +1,16 @@
+// Prefer WPF types when WinForms/System.Drawing are also referenced (tray icon).
+global using Application = System.Windows.Application;
+global using Color = System.Windows.Media.Color;
+global using Point = System.Windows.Point;
+global using Size = System.Windows.Size;
+global using Image = System.Windows.Controls.Image;
+global using Brushes = System.Windows.Media.Brushes;
+global using Button = System.Windows.Controls.Button;
+global using CheckBox = System.Windows.Controls.CheckBox;
+global using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+global using MouseEventArgs = System.Windows.Input.MouseEventArgs;
+global using Cursors = System.Windows.Input.Cursors;
+global using UserControl = System.Windows.Controls.UserControl;
+global using Orientation = System.Windows.Controls.Orientation;
+global using MessageBox = System.Windows.MessageBox;
+global using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
